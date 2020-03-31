ff4ee5ed1bc82c4143e76b98da65a86f9ddc3aa5
ensallee <ensallee@gmail.com>
2020-03-31 14:02:09 -0400

    remove unused CSS

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, { useState, useEffect } from "react";
-import Container from "./Container";
-import "./App.css";
+import React, { useState, useEffect } from "react"
+import Container from "./Container"
 
 const initialLocation = {
   latitude: 40.68,
   longitude: -73.97
-};
+}
 
 function App() {
-  const [location, setLocation] = useState(initialLocation);
+  const [location, setLocation] = useState(initialLocation)
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(handlePosition);
-  }, []); //TODO: call useEffect when there has been a change in location.
+    navigator.geolocation.getCurrentPosition(handlePosition)
+  }, []) //TODO: call useEffect when there has been a change in location.
 
   const handlePosition = e => {
     setLocation({
       latitude: e.coords.latitude,
       longitude: e.coords.longitude
-    });
-  };
+    })
+  }
 
-  return <Container location={location} setLocation={setLocation} />;
+  return <Container location={location} setLocation={setLocation} />
 }
 
-export default App;
+export default App
